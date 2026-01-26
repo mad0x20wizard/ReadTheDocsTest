35b8e9e42ef770b885575a413d60f1127b934f75
Kevin Koslowski <kevin@madwizard.de>
2026-01-26 14:06:27 +0100

    test styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,9 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('custom.css')   # Sphinx 1.8+
+    # app.add_js_file('custom.js')   # if you want JS too
